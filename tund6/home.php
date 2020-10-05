a216fd703d09c327f551027cfd33f90412792499
Andrus Rinde <andrusrinde@users.noreply.github.com>
2020-10-05 14:14:52 +0300

    Viies kodutöö tehtud

---
+++ tund6/home.php
@@ -0,0 +1,26 @@
+<?php
+  $username = "Andrus Rinde";
+  
+  require("header.php");
+?>
+
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse bänner">
+  <h1><?php echo $username; ?> programmeerib veebi</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+  
+  <ul>
+    <li><a href="listideas.php">Mõtete näitamine</a></li>
+	<li><a href="addideas.php">Mõtete lisamine</a></li>
+	<li><a href="listfilms.php">Filmiinfo näitamine</a></li>
+	<li><a href="addfilms.php">Filmiinfo lisamine</a></li>
+  </ul>
+  
+</body>
+</html>
+
+
+
+
+
+
